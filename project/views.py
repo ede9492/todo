b4343f562703bc9e2fe9c5aef07ef48c437ce66c
ede9492 <officialearnings26@gmail.com>
2024-03-03 22:49:36 +0530

    first commit

---
+++ project/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from myapp.models import Task
+
+
+def home(request):
+    tasks=Task.objects.filter(is_completed=False)
+    completed_tasks=Task.objects.filter(is_completed=True)
+    context={
+        'tasks':tasks,
+        'completed_tasks':completed_tasks
+        }
+    
+    return render(request,'home.html',context)
